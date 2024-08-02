6e3766bd62b0d5143a87006a3d362bd22aa23847
sayed younis <elsaydseif17@gmail.com>
2024-08-02 19:05:15 +0300

    edit validation

--- lib/presentation/Views/add_password_screen.dart
+++ lib/presentation/Views/add_password_screen.dart
@@ -1,14 +1,18 @@
 import 'dart:developer';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:password_saver/constants.dart';
 import 'package:password_saver/cubit/add_password_cubit/add_password_cubit.dart';
 import 'package:password_saver/cubit/show_password_cubit/show_password_cubit.dart';
+import 'package:password_saver/data/models/image_model.dart';
 import 'package:password_saver/data/models/password_model.dart';
-import 'package:password_saver/widgets/custom_text.dart';
-import 'package:password_saver/widgets/custom_button.dart';
-import 'package:password_saver/widgets/custom_text_feild.dart';
+import 'package:password_saver/presentation/widgets/custom_text.dart';
+import 'package:password_saver/presentation/widgets/custom_button.dart';
+import 'package:password_saver/presentation/widgets/custom_text_feild.dart';
 
 class AddPasswordScreen extends StatefulWidget {
   const AddPasswordScreen({super.key});
@@ -21,12 +25,11 @@ class AddPasswordScreen extends StatefulWidget {
 class _AddPasswordScreenState extends State<AddPasswordScreen> {
   String? title, pass, email, url, userName;
   final _formKey = GlobalKey<FormState>();
+  ////File? selectedImage;
 
   @override
   void initState() {
     super.initState();
-    BlocProvider.of<AddPasswordCubit>(context).addPassword;
-    // BlocProvider.of<ShowPasswordCubit>(context);
   }
 
   @override
@@ -51,6 +54,9 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
                 BlocProvider.of<ShowPasswordCubit>(context).showpassword();
                 Navigator.pop(context);
               }
+              if (state is AddPasswordFailure) {
+                log(state.errMessage.toString());
+              }
             },
             builder: (context, state) {
               return Column(
@@ -72,11 +78,13 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
                         const CustomText(text: 'title'),
                         CustomTextFeild(
                           validator: (value) {
-                            if (value==null||value.isEmpty) {
+                            if (value == null || value.isEmpty) {
                               return 'thisfeild is required';
                             }
                             if (value.length >= 10) {
                               return 'only accept 10 characters';
+                            } else {
+                              return null;
                             }
                           },
                           hintText: 'Website/AppName',
@@ -87,6 +95,13 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
                         ),
                         const CustomText(text: 'Url'),
                         CustomTextFeild(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'this feild is required';
+                            } else {
+                              return null;
+                            }
+                          },
                           hintText: 'Website Url',
                           onChange: (val) {
                             url = val;
@@ -95,6 +110,13 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
                         ),
                         const CustomText(text: 'email'),
                         CustomTextFeild(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'this feild is required';
+                            } else {
+                              return null;
+                            }
+                          },
                           hintText: 'example@mail.com',
                           onChange: (val) {
                             email = val;
@@ -103,6 +125,13 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
                         ),
                         const CustomText(text: 'user_name'),
                         CustomTextFeild(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'this feild is required';
+                            } else {
+                              return null;
+                            }
+                          },
                           hintText: 'user_name',
                           onChange: (val) {
                             userName = val;
@@ -111,6 +140,13 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
                         ),
                         const CustomText(text: 'password'),
                         CustomTextFeild(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'this feild is required';
+                            } else {
+                              return null;
+                            }
+                          },
                           hintText: 'password',
                           onChange: (val) {
                             pass = val;
@@ -143,21 +179,42 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
                       ),
                     ],
                   ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                    //// _pickImage();
+                    },
+                    child: Container(
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        color: Colors.grey,
+                      ),
+                    //  // child: selectedImage != null
+                    //  //     ? Image.file(
+                    //  //         selectedImage!,
+                    //  //         fit: BoxFit.cover,
+                    //  //       )
+                    //  //     : const Icon(Icons.add),
+                    ),
+                  ),
                   CustomButton(
                     text: 'ADD PASSWORD',
-                    onTaP: () {
+                    onTaP: () async{
                       if (_formKey.currentState!.validate()) {
-                        BlocProvider.of<AddPasswordCubit>(context)
-                            .addPassword(PasswordModel(
+                        PasswordModel passwordModel = PasswordModel(
                           title: title!,
                           password: pass!,
                           email: email!,
                           userName: userName!,
                           url: url,
-                        ));
-                        BlocProvider.of<AddPasswordCubit>(context);
-                      } else {
-                        return null;
+                          // image: ImageModel(imageData:await fileToUint8List(selectedImage!)),
+                        );
+                        BlocProvider.of<AddPasswordCubit>(context)
+                            .addPassword(passwordModel);
                       }
                     },
                   ),
@@ -169,4 +226,18 @@ class _AddPasswordScreenState extends State<AddPasswordScreen> {
       ),
     );
   }
+
+// // Function to convert File to Uint8List
+//// Future<Uint8List> fileToUint8List(File file) async {
+//   return await file.readAsBytes();
+// }
+
+////   Future _pickImage() async {
+//     var returnedImage =
+//         await ImagePicker().pickImage(source: ImageSource.gallery);
+//     setState(() {
+//       selectedImage = File(returnedImage!.path);
+//     });
+//   }
+
 }
