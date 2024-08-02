474bca5e76e7833c7fb834cf886ab57e3fb09fa8
sayed younis <elsaydseif17@gmail.com>
2024-08-02 14:33:56 +0300

    fixing note don't delete imediatly & copy password using button

--- lib/widgets/custom_password_card.dart
+++ lib/widgets/custom_password_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:password_saver/cubit/add_password_cubit/add_password_cubit.dart';
+import 'package:password_saver/cubit/show_password_cubit/show_password_cubit.dart';
 import 'package:password_saver/data/models/password_model.dart';
 import 'package:password_saver/widgets/custom_text.dart';
 
-class CustomPasswordCard extends StatelessWidget {
+class CustomPasswordCard extends StatefulWidget {
   const CustomPasswordCard({
     super.key,
     required this.password,
@@ -12,72 +14,100 @@ class CustomPasswordCard extends StatelessWidget {
   final PasswordModel password;
 
   @override
+  State<CustomPasswordCard> createState() => _CustomPasswordCardState();
+}
+
+class _CustomPasswordCardState extends State<CustomPasswordCard> {
+  @override
   Widget build(BuildContext context) {
     return BlocBuilder<AddPasswordCubit, AddPasswordState>(
       builder: (context, state) {
-        return GestureDetector(
-          onLongPress: () async {
-            return showDialog<void>(
-              context: context,
-              barrierDismissible: false, // user must tap button!
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Delete Password'),
-                  content: const SingleChildScrollView(
-                    child: ListBody(
-                      children: <Widget>[
-                        Text('Are you sure you want to delete this password ?'),
+        return BlocConsumer<AddPasswordCubit, AddPasswordState>(
+          listener: (BuildContext context, AddPasswordState state) {
+            if (state is AddPasswordSuccess) {
+              BlocProvider.of<AddPasswordCubit>(context)
+                  .addPassword(widget.password);
+            }
+          },
+          builder: (context, state) {
+            return GestureDetector(
+              onLongPress: () async {
+                return showDialog<void>(
+                  context: context,
+                  barrierDismissible: false, // user must tap button!
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Delete Password'),
+                      content: const SingleChildScrollView(
+                        child: ListBody(
+                          children: <Widget>[
+                            Text(
+                                'Are you sure you want to delete this password ?'),
+                          ],
+                        ),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          child: const Text('Yes'),
+                          onPressed: () {
+                            widget.password.delete();
+                            BlocProvider.of<ShowPasswordCubit>(context)
+                                .showpassword();
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        TextButton(
+                          child: const Text('No'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
                       ],
-                    ),
-                  ),
-                  actions: <Widget>[
-                    TextButton(
-                      child: const Text('Delete'),
-                      onPressed: () {
-                        password.delete();
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ],
+                    );
+                  },
                 );
               },
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all()),
+                margin: const EdgeInsetsDirectional.only(top: 20),
+                padding: const EdgeInsetsDirectional.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: Image.asset('assets/images/facebook.png'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: SizedBox(
+                        width: 100,
+                        child: CustomText(
+                          text: widget.password.title,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      icon: const Icon(
+                        Icons.copy,
+                        color: Colors.teal,
+                      ),
+                      onPressed: () {
+                        Clipboard.setData(
+                            ClipboardData(text: widget.password.toString()));
+                      },
+                    )
+                  ],
+                ),
+              ),
             );
           },
-          child: Container(
-            height: 80,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20), border: Border.all()),
-            margin: const EdgeInsetsDirectional.only(top: 20),
-            padding: const EdgeInsetsDirectional.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: Image.asset('assets/images/facebook.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: SizedBox(
-                    width: 100,
-                    child: CustomText(
-                      text: password.title,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const Spacer(),
-                IconButton(
-                  icon: const Icon(
-                    Icons.copy,
-                    color: Colors.teal,
-                  ),
-                  onPressed: () {},
-                )
-              ],
-            ),
-          ),
         );
       },
     );
