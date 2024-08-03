f3de87310cd05a745816c954b92b68c74e501990
sayed younis <elsaydseif17@gmail.com>
2024-08-03 07:46:00 +0300

    adding new fonts

--- lib/presentation/widgets/custom_text.dart
+++ lib/presentation/widgets/custom_text.dart
@@ -8,12 +8,14 @@ class CustomText extends StatelessWidget {
     this.fontWeight,
     this.textColor,
     this.maxLines,
+    this.fontfamily,
   });
   final String text;
   final double? fontSize;
   final FontWeight? fontWeight;
   final Color? textColor;
   final int? maxLines;
+  final String? fontfamily;
   @override
   Widget build(BuildContext context) {
     return Text(
@@ -21,6 +23,7 @@ class CustomText extends StatelessWidget {
       overflow: TextOverflow.ellipsis,
       text,
       style: TextStyle(
+        fontFamily: fontfamily,
         fontSize: fontSize ?? 16,
         fontWeight: fontWeight ?? FontWeight.normal,
         color: textColor ?? Colors.black,
